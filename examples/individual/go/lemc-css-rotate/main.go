0b0a488ee8a22f6b6edd895eabac253b6684a37a
Jared Folkins <jfolkins@gmail.com>
2025-05-17 08:50:35 -0700

    fix: Adjust sleep duration in color rotation loop for improved rendering speed and update print statement for consistency in output formatting.

--- examples/individual/go/lemc-css-rotate/main.go
+++ examples/individual/go/lemc-css-rotate/main.go
@@ -23,20 +23,19 @@ func main() {
 
 	arr := [8]string{"#ffadad", "#ffd6a5", "#fdffb6", "#caffbf", "#9bf6ff", "#a0c4ff", "#bdb2ff", "#ffc6ff"}
 
-	fmt.Println("lemc.html.trunc;")
+	fmt.Printf("lemc.html.trunc;\n")
 
 	shuffleArray(arr[:])
 	for i := 0; i < 3; i++ {
 		for _, color := range arr {
 			fmt.Printf("lemc.css.trunc; #%s { background-color: %s; }\n", htmlid, color)
 			fmt.Printf("lemc.html.append; %s<br>\n", color)
-			time.Sleep(100 * time.Millisecond)
+			time.Sleep(50 * time.Millisecond)
 		}
 	}
 }
 
 func shuffleArray(arr []string) {
-	rand.Seed(time.Now().UnixNano())
 	rand.Shuffle(len(arr), func(i, j int) {
 		arr[i], arr[j] = arr[j], arr[i]
 	})
