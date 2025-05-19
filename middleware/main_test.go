1000f0289ee3c3adc15e5d219d3c6a0f0ac7a47a
Jared Folkins <jfolkins@gmail.com>
2025-05-19 16:09:57 -0700

    Add TestMain setups and data directory

---
+++ middleware/main_test.go
@@ -0,0 +1,19 @@
+package middleware
+
+import (
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/jaredfolkins/letemcook/tests/testutil"
+)
+
+func TestMain(m *testing.M) {
+	dataRoot := testutil.DataRoot()
+	os.Setenv("LEMC_ENV", "test")
+	os.Setenv("LEMC_DATA", dataRoot)
+	envDir := filepath.Join(dataRoot, "test")
+	_ = os.MkdirAll(envDir, 0o755)
+	code := m.Run()
+	os.Exit(code)
+}
